05326f87fd5f806295f42351068a9b2b78292d72
Cunillera-Montcusi <david.cunillera@dcm.cat>
2025-05-10 06:56:50 +0200

    Install packaages

---
+++ PackageInstallation.R
@@ -0,0 +1,24 @@
+
+# Geographic tools (distances based on lat/long data) 
+install.packages("geosphere")
+
+# Plot and data management
+install.packages("viridis")
+install.packages("tidyverse")
+
+# Network-focused mostly used packages 
+install.packages("sna")
+install.packages("igraph")
+install.packages("bipartite")
+
+#* Bonus for some specific structures (Mimimum spanning tree)
+install.packages("ape")
+
+# Network representation
+install.packages("ggnetwork")
+
+# Plot and data management
+install.packages("viridis")
+install.packages("tidyverse")
+install.packages("gridExtra")
+install.packages("vegan")
